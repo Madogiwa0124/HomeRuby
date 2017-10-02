91a463b73c50dec8c9cc0554c19997f5d651e0c0
Madogiwa <17684091+Madogiwa0124@users.noreply.github.com>
2017-10-02 23:39:04 +0900

    initialize

---
+++ HomeRuby.rb
@@ -0,0 +1,24 @@
+require 'yaml'
+
+class Home
+  attr_reader :messages
+
+  def initialize(cnt)
+    @homessages = yaml_load
+    @messages = create_homessages(cnt)
+  end
+
+  def create_homessages(cnt)
+    messages = []
+    cnt.times do |i|
+      messages << @homessages.sample
+    end
+    messages
+  end
+
+  def yaml_load
+    data = open('Homessages.yml', 'r') { |f| YAML.load(f) }
+  end
+end
+
+puts Home.new(ARGV[0].to_i).messages;
